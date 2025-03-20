b8f96cf7df4d893c4ab32f6f43c97790d2f9985d
heman <heman@LAPTOP-ILEVVRC8>
2025-03-20 18:26:29 +0530

    hi

---
+++ test/Run.bat
@@ -0,0 +1,4 @@
+cd C:\Users\heman\eclipse-workspace.java\test
+mvn verify
+mvn compile
+mvn test
---
+++ test/src/test/java/test/script.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class script {
+	@Test
+	public void scrip()
+	{
+		System.out.println("***********************hemanth************************");
+	}
+}
